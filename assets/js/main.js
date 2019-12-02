8a6b10409f297c2e7a4c4a492780ed2e4e3b32c7
cucurbiturilum <cucurbiturilum@gmail.com>
2019-12-02 16:57:34 +0200

    add toggle chat panel

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var chatPanelToggle = $('#chat-panel-toggle');
+    var chatPanel = $('#chat-panel');
+
+    chatPanel.hide();
+
+    chatPanelToggle.click(function () {
+        chatPanel.toggle();
+    })
+})
