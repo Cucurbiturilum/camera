3f046842476abf4b29efa1153d93ea58a1027da1
cucurbiturilum <cucurbiturilum@gmail.com>
2019-12-04 01:08:26 +0200

    added screen share modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,18 @@ $(document).ready(function () {
     $("#emoji").emojioneArea();
 
     $('[data-toggle="tooltip"]').tooltip();
+
+    var screenShare = $('#screen-share')
+    var screenShareModal = $('#screen-share-modal');
+    var screnShareModalClose = $('#screen-share-modal-close');
+
+    screenShareModal.hide();
+
+    screenShare.click(function () {
+        screenShareModal.toggle();
+    })
+
+    screnShareModalClose.click(function(){
+        screenShareModal.hide();
+    });
 })
